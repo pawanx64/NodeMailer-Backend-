b432b9984ddad01c7b833c423959001bf70120ff
Pawan Kumar <pawankumar.nov5.com>
2024-07-15 15:20:54 +0530

    first commit

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use('/api/auth', authRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send(`<h1>This Is HomePage</h1>`);
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
